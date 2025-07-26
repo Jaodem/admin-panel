65a1a7c81ef4a98e56a70ee182f681e1f85ef74c
Jao <109613248+Jaodem@users.noreply.github.com>
2025-07-25 21:09:49 -0300

    feat: login funcional con generación de token JWT

--- admin-backend/controllers/authController.js
+++ admin-backend/controllers/authController.js
@@ -1,5 +1,6 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
-import bcrypt from 'bcrypt';
 
 export const registerUser = async (req, res) => {
     try {
@@ -24,5 +25,41 @@ export const registerUser = async (req, res) => {
     } catch (error) {
         console.error('Error en registerUser:', error);
         res.status(500).json({ message: 'Error al registrar el usuario' });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    // Válidación básica
+    if (!email || !password) return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) return res.status(404).json({ message: 'Usuario no encontrado' });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) return res.status(401).json({ message: 'Credenciales inválidas' });
+
+        // Se genera el toke
+        const token = jwt.sign(
+            { userId: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '2h'}
+        );
+
+        res.status(200).json({
+            message: 'Login exitoso',
+            token,
+            user: {
+                _id: user._id,
+                username: user.username
+            }
+        });
+    } catch (error) {
+        console.error('Error al iniciar sesión:', error);
+        res.status(500).json({ message: 'Error en el servidor', error: error.message });
     }
 }
